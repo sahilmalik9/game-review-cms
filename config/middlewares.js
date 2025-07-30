a370a797cf7351945678638a9705dad6c8692160
sahilmalik9 <sahilmalik9390@gmail.com>
2025-07-30 12:05:10 -0400

    Initial commit

---
+++ config/middlewares.js
@@ -0,0 +1,19 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173', 'http://localhost:5176'], // Add both in case the port changes
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
